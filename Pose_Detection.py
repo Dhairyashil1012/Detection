f2db4f3601f9da2f9935c3175bfb6acc610759cf
Dhairyashil1012 <dknarute1012@gmail.com>
2026-05-05 13:29:10 +0530

    initial commit

---
+++ Pose_Detection.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+import os
+# Load YOLOv11 pose model
+model = YOLO("model/yolo11n-pose.pt")
+
+
+# results = model("D:\\Photos Storage_p\\onkar and me\\IMG-20250121-WA0022.jpg")
+# Ask user to paste the path
+image_path = input("Enter the path to your image: ").strip('"')
+
+
+
+results = model(image_path)
+
+
+for result in results:
+    result.save(filename=os.path.join("output", "output.jpg"))
+
+
+print("Pose detection completed. Check output.jpg")
